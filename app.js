dbaa334f43fe8e41092e0946987d6dc0bb274066
ZeltGG <jairo.urbina@hotmail.com>
2025-07-02 23:33:19 -0500

    final github

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
-console.log('🟢 app.js se está ejecutando');
-require('dotenv').config(); 
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
-
-
+git
 app.get('/', (req, res) => {
   res.send('Servidor backend funcionando');
 });
 
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch(err => console.error('Error al conectar con MongoDB:', err));
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('✅ Conectado a MongoDB'))
-.catch(err => console.error('❌ Error al conectar con MongoDB:', err));
-
-// Arrancar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-console.log('URI de MongoDB:', process.env.MONGODB_URI);
